03778439064a1081b5a929204d7f63817919169a
Tim Scott <0timothy.scott0@gmail.com>
2022-09-21 22:48:28 +0000

    add dummy about page

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const About: NextPage = () => {
+  return (
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde eaque excepturi repudiandae et magnam. Nobis dicta quia rerum omnis voluptate doloribus in laudantium accusantium quis consequatur, ad recusandae tenetur nemo fugiat hic suscipit exercitationem dignissimos molestias ipsum ut. Fuga nemo corrupti non pariatur mollitia minima id adipisci debitis, temporibus ratione dolores qui facere aspernatur omnis. Consequatur tenetur tempora corrupti nostrum vel architecto minima sed veniam, nam odit facilis atque laborum placeat labore reiciendis, ipsum, quis iusto! Dolorum, est quaerat. Expedita fuga, nobis illum aut harum similique neque dignissimos incidunt ipsum veritatis repellendus fugiat voluptatum error, eveniet, dolorem eos inventore iste.
+    </p>
+  )
+}
+
+export default About
