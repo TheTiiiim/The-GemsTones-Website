a663c58a02acb68a3b86dd60156e3ea4454ca080
thetiiiim <0timothy.scott0@gmail.com>
2023-02-01 19:33:15 -0500

    gallery boilerplate files

---
+++ pages/gallery.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+
+import styles from '../styles/Gallery.module.css'
+import Image from 'next/image'
+
+const About: NextPage = () => {
+  return (
+    <div>
+      <h1>Songs</h1>
+      <h2></h2>
+    </div>
+  )
+}
+
+export default About
