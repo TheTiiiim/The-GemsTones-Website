6e65f0dbe688b4845eb508bb05eaea3203e1f0a4
thetiiiim <0timothy.scott0@gmail.com>
2023-02-01 19:15:37 -0500

    add banner to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,23 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Iframe from 'react-iframe'
-import logo from '../public/logo.jpg'
+import banner from '../public/banner.jpg'
 
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.featuredContainer1}>
-      <div className={styles.featuredContainer2}>
-        <Iframe 
-          url="https://www.youtube.com/embed/FYSj5IUTe54"
-          className={styles.featured}
-        />
+    <div className={styles.center}>
+      <Image 
+        src={banner}
+        alt="The GemsTones Banner"
+      />
+      <div className={styles.featuredContainer1}>
+        <div className={styles.featuredContainer2}>
+          <Iframe 
+            url="https://www.youtube.com/embed/FYSj5IUTe54"
+            className={styles.featured}
+          />
+        </div>
       </div>
     </div>
   )
