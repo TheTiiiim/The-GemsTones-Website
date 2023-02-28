1d73170eb39e06ebe5d349bc0b41e1da8cc4cf50
thetiiiim <0timothy.scott0@gmail.com>
2023-02-28 11:58:05 -0500

    blank dates page

---
+++ pages/dates.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+import styles from '../styles/Dates.module.css'
+
+const About: NextPage = () => {
+  return (
+    <div>
+      <h1>Dates</h1>
+    </div>
+  )
+}
+
+export default About
