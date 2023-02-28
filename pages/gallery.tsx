7703829b440ee475069076d6a585983a35fe3b67
thetiiiim <0timothy.scott0@gmail.com>
2023-02-28 17:25:54 -0500

    put preview in gallery

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from 'next'
 
 import styles from '../styles/Gallery.module.css'
+import streamingservicepreview from '../public/streamingservicepreview.jpg'
 import Image from 'next/image'
 
 const Gallery: NextPage = () => {
   return (
     <div>
       <h1>Songs</h1>
-      <h2></h2>
+      <Image 
+        src={streamingservicepreview}
+        alt="The Gemstones is coming to your favorite services starting March 10th."
+      />  
     </div>
   )
 }
