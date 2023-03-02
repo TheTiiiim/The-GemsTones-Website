0450e6c0b9e8c72dd61de3d42ffc9b771b0f13bd
thetiiiim <0timothy.scott0@gmail.com>
2023-03-02 01:43:31 -0500

    let next/head handle viewport and other meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/syq1pzk.css" />
-        <meta name="description" content="The GemsTones is a retro-rock band with a love for all things nostalgia. Bringing back the biggest hits of the 50's." />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/favicon.ico"></link>
       </Head>
       <body>
         <NextScript />
