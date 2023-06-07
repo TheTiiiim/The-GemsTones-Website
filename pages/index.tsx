a37ef7c1850a81a41cb44155b1a6d22ef9106cc6
Tim Scott <0timothy.scott0@gmail.com>
2023-06-07 18:19:01 -0400

    add new banner and adjust its layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Iframe from 'react-iframe'
 
 import styles from '../styles/Home.module.css'
-import banner from '../public/banner.jpg'
+import banner from '../public/banner2.png'
 
 
 const Home: NextPage = () => {
@@ -14,11 +14,15 @@ const Home: NextPage = () => {
         <title>Home - The GemsTones 50&apos;s Classics</title>
       </Head>
 
-      <h1>
-        <Image 
-          src={banner}
-          alt="The GemsTones 50&apos;s Classics"
-        />
+      <h1 className={styles.bannerHeader}>
+        <div className={styles.bannerContainer}>
+          <Image 
+            layout="fill"
+            objectFit="contain"
+            src={banner}
+            alt="The GemsTones 50&apos;s Classics"
+          />
+        </div>
       </h1>
       <div className={styles.featuredContainer1}>
         <div className={styles.featuredContainer2}>
