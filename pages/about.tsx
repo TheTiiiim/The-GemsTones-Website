60b1d7d1ff643fbfe9f37e746960dce8f7d97d3a
Tim Scott <0timothy.scott0@gmail.com>
2023-06-23 10:32:39 -0400

    update bios and photos

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,10 +4,10 @@ import styles from '../styles/About.module.css'
 import Image from 'next/image'
 import Head from 'next/head'
 
-import angsty_amethyst from '../public/angsty-amethyst.jpg'
-import electric_emerald from '../public/electric-emerald.jpg'
-import rockin_ruby from '../public/rockin-ruby.jpg'
-import swingin_sapphire from '../public/swingin-sapphire.jpg'
+import angsty_amethyst from '../public/images/Hunter_Logo_003.jpg'
+import electric_emerald from '../public/images/Mark_Logo_004.jpg'
+import rockin_ruby from '../public/images/Joe_Logo_003.jpg'
+import swingin_sapphire from '../public/images/Jett_Logo_003.jpg'
 
 const About: NextPage = () => {
   return (
@@ -36,7 +36,7 @@ const About: NextPage = () => {
         <div className={styles.split}>
           <div className={styles.description}>
             <h2>Swingin&apos; Sapphire</h2>
-            <p>Keys, Acoustic Guitar, Lead & Backing Vocals; Swingin&apos; Sapphire aka Jett haryslak is the youngest member of The GemsTones and brother of fellow members Hunter and Josef. Jett&apos;s influncess include class acts such as Queen, The Beatles, Let Zeppelin, and Electric Light Orchestra; and more obscure acts such as Will Wood and Tally Hall. Jett spent his high school years acting in plays and performing in the jazz band. He looks forward to bringing his energetic personality into our nostalgic performances.</p>
+            <p>Keys, Acoustic Guitar, Lead & Backing Vocals; Swingin&apos; Sapphire aka Jett Haryslak is the youngest member of The GemsTones and brother of fellow members Hunter and Josef. Jett&apos;s influences include classic rock acts such as Queen, The Beatles, Led Zeppelin, and Electric Light Orchestra; and more obscure acts such as Will Wood and Tally Hall. Jett spent his high school years acting in plays and performing in the jazz band. He looks forward to bringing his energetic personality into our nostalgic performances.</p>
           </div>  
           <div className={styles.portraitContainer}>
             <Image 
@@ -50,7 +50,7 @@ const About: NextPage = () => {
         <div className={styles.split}>
           <div className={styles.description}>
             <h2>Electric Emerald</h2>
-            <p>Drums & Backing Vocals; Electric Emerald aka Mark Scott is a longtime gospel drummer and multi-instrumentalist with a specialty in sizzling high harmonies. And while he may not be related to the other GemsTones members, he is our drummer from another mother. With inspiration from Crosby Stills & Nash, Earth Wind & Fire, and Paul Simon; he is here to drop the beat and keep you on your feet!</p>
+            <p>Drums & Backing Vocals; Electric Emerald aka Mark Scott is a longtime gospel drummer and multi-instrumentalist with a specialty in sizzling high harmonies. And while he may not be related to other GemsTones members, he is our drummer from another mother. With inspiration from Crosby Stills & Nash, Earth Wind & Fire, and Paul Simon; he is here to drop the beat and keep you on your feet!</p>
           </div>
           <div className={styles.portraitContainer}>
             <Image 
@@ -64,7 +64,7 @@ const About: NextPage = () => {
         <div className={styles.split}>
           <div className={styles.description}>
             <h2>Angsty Amethyst</h2>
-            <p>Bassist and Graphic artist; Angsty Amethyst has been playing guitar and writing music for over 10 years. He has even self produced two solo albums. He&apos;s also the middle brother of fellow GemsTones members Josef and Jett. Hunter&apos;s influences included Johnny Cash, John Lennon, Shaun Morgan and Bob Seger. Hunter has always had a thing for the angst aesthetic, but don&apos;t let that tough exterior fool you. There is a warm heart under all that cool leather.</p>
+            <p>Bassist, Low Vocals and Graphic Artist; Angsty Amethyst aka Hunter Germaine has been playing guitar and writing music for over 10 years. He has even self produced two solo albums. He&apos;s also the middle brother of fellow GemsTones members Josef and Jett. Hunter’s influences include Johnny Cash, John Lennon, Shaun Morgan, Bob Seger, and Cat Stevens. Hunter has always had a thing for the angst aesthetic, but don&apos;t let that tough exterior fool you. There is a warm heart under all that cool leather.</p>
           </div>
           <div className={styles.portraitContainer}>
             <Image 
