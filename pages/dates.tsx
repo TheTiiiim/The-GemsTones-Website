d1558b17c98b28aae4be9245683c5b896228c221
Tim Scott <0timothy.scott0@gmail.com>
2023-07-20 00:27:27 -0400

    Update Events

--- pages/dates.tsx
+++ pages/dates.tsx
@@ -60,24 +60,6 @@ const Dates: NextPage = () => {
         </li>
         
         <li>
-          <div className={styles.dateTime}><h2>July 29, 2023</h2>&#32;<span className={styles.time}>(6:00pm - 9:00pm)</span></div>
-          <p className={styles.venue}>Elk&apos;s Lodge</p>
-          <p className={styles.tickets}>Tickets at the door - $20</p>
-          <p className={styles.cityState}>Nashua, NH</p>
-          <p className={styles.address}>12 Murphy Dr</p>
-          <div className={styles.directionsButton}><a target="_blank" href="https://goo.gl/maps/vRPCwRgyydqkY8C39" rel="noreferrer">Directions</a></div>
-        </li>
-        
-        <li>
-          <div className={styles.dateTime}><h2>August 12, 2023</h2>&#32;<span className={styles.time}>(7:00pm - 10:00pm)</span></div>
-          <p className={styles.venue}>Elk&apos;s Lodge</p>
-          <p className={styles.tickets}>Tickets at the door - $20</p>
-          <p className={styles.cityState}>Tewksbury, MA</p>
-          <p className={styles.address}>777 South St</p>
-          <div className={styles.directionsButton}><a target="_blank" href="https://goo.gl/maps/AkiqkiosQSxAdyoD7" rel="noreferrer">Directions</a></div>
-        </li>
-        
-        <li>
           <div className={styles.dateTime}><h2>August 17, 2023</h2>&#32;<span className={styles.time}>(6:00pm - 8:00pm)</span></div>
           <p className={styles.venue}>Millyard Amphitheater</p>
           <p className={styles.tickets}>Free entry</p>
@@ -105,15 +87,6 @@ const Dates: NextPage = () => {
         </li>
         
         <li>
-          <div className={styles.dateTime}><h2>September 16, 2023</h2>&#32;<span className={styles.time}>(7:00pm - 10:00pm)</span></div>
-          <p className={styles.venue}>Elk&apos;s Lodge</p>
-          <p className={styles.tickets}>Tickets at the door - $20</p>
-          <p className={styles.cityState}>Wakefield, MA</p>
-          <p className={styles.address}>63 Bay State Rd</p>
-          <div className={styles.directionsButton}><a target="_blank" href="https://goo.gl/maps/ot6BEGqPQ9RfHrDd9" rel="noreferrer">Directions</a></div>
-        </li>
-        
-        <li>
           <div className={styles.dateTime}><h2>October 7, 2023</h2>&#32;<span className={styles.time}>(12:00pm - 3:00pm)</span></div>
           <p className={styles.venue}>Cider Hill Farm</p>
           <p className={styles.tickets}>Free entry</p>
@@ -134,16 +107,25 @@ const Dates: NextPage = () => {
         <li>
           <div className={styles.dateTime}><h2>October 14, 2023</h2>&#32;<span className={styles.time}>(7:00pm - 10:00pm)</span></div>
           <p className={styles.venue}>Elk&apos;s Lodge</p>
-          <p className={styles.tickets}>Tickets at the door - $15</p>
+          <p className={styles.tickets}>Tickets at the door - $10</p>
           <p className={styles.cityState}>Keene, NH</p>
           <p className={styles.address}>81 Roxbury St</p>
           <div className={styles.directionsButton}><a target="_blank" href="https://goo.gl/maps/Y5DxbAzFnBPu28dn8" rel="noreferrer">Directions</a></div>
         </li>
         
         <li>
+          <div className={styles.dateTime}><h2>October 15, 2023</h2>&#32;<span className={styles.time}>(7:00pm - 10:00pm)</span></div>
+          <p className={styles.venue}>Newburyport Brewing Company</p>
+          <p className={styles.tickets}>Free entry</p>
+          <p className={styles.cityState}>Newburyport, MA</p>
+          <p className={styles.address}>4 New Pasture Rd</p>
+          <div className={styles.directionsButton}><a target="_blank" href="https://goo.gl/maps/CfvAmykhjDqbco87A" rel="noreferrer">Directions</a></div>
+        </li>
+        
+        <li>
           <div className={styles.dateTime}><h2>October 28, 2023</h2>&#32;<span className={styles.time}>(6:00pm - 9:00pm)</span></div>
           <p className={styles.venue}>Elk&apos;s Lodge</p>
-          <p className={styles.tickets}>Tickets at the door - $15</p>
+          <p className={styles.tickets}>Tickets at the door - $10</p>
           <p className={styles.cityState}>Manchester, NH</p>
           <p className={styles.address}>290 Granite St</p>
           <div className={styles.directionsButton}><a target="_blank" href="https://goo.gl/maps/5QPx9PPgQpgP69s18" rel="noreferrer">Directions</a></div>
@@ -152,43 +134,16 @@ const Dates: NextPage = () => {
         <li>
           <div className={styles.dateTime}><h2>November 18, 2023</h2>&#32;<span className={styles.time}>(6:00pm - 9:00pm)</span></div>
           <p className={styles.venue}>Elk&apos;s Lodge</p>
-          <p className={styles.tickets}>Tickets at the door - $20</p>
+          <p className={styles.tickets}>Tickets at the door - $10</p>
           <p className={styles.cityState}>Nashua, NH</p>
           <p className={styles.address}>12 Murphy Dr</p>
           <div className={styles.directionsButton}><a target="_blank" href="https://goo.gl/maps/vRPCwRgyydqkY8C39" rel="noreferrer">Directions</a></div>
         </li>
         
         <li>
-          <div className={styles.dateTime}><h2>November 25, 2023</h2>&#32;<span className={styles.time}>(6:00pm - 9:00pm)</span></div>
-          <p className={styles.venue}>Elk&apos;s Lodge</p>
-          <p className={styles.tickets}>Tickets at the door - $20</p>
-          <p className={styles.cityState}>Dover, NH</p>
-          <p className={styles.address}>282 Durham Rd</p>
-          <div className={styles.directionsButton}><a target="_blank" href="https://goo.gl/maps/BcBUASvHGX7iaZ3N8" rel="noreferrer">Directions</a></div>
-        </li>
-        
-        <li>
-          <div className={styles.dateTime}><h2>December 2, 2023</h2>&#32;<span className={styles.time}>(7:00pm - 10:00pm)</span></div>
-          <p className={styles.venue}>Elk&apos;s Lodge</p>
-          <p className={styles.tickets}>Tickets at the door - $20</p>
-          <p className={styles.cityState}>Wakefield, MA</p>
-          <p className={styles.address}>63 Bay State Rd</p>
-          <div className={styles.directionsButton}><a target="_blank" href="https://goo.gl/maps/ot6BEGqPQ9RfHrDd9" rel="noreferrer">Directions</a></div>
-        </li>
-        
-        <li>
-          <div className={styles.dateTime}><h2>December 9, 2023</h2>&#32;<span className={styles.time}>(7:00pm - 10:00pm)</span></div>
-          <p className={styles.venue}>Elk&apos;s Lodge</p>
-          <p className={styles.tickets}>Tickets at the door - $20</p>
-          <p className={styles.cityState}>Tewksbury, MA</p>
-          <p className={styles.address}>777 South St</p>
-          <div className={styles.directionsButton}><a target="_blank" href="https://goo.gl/maps/AkiqkiosQSxAdyoD7" rel="noreferrer">Directions</a></div>
-        </li>
-        
-        <li>
           <div className={styles.dateTime}><h2>December 23, 2023</h2>&#32;<span className={styles.time}>(3:00pm - 6:00pm)</span></div>
           <p className={styles.venue}>Elk&apos;s Lodge</p>
-          <p className={styles.tickets}>Tickets at the door - $20</p>
+          <p className={styles.tickets}>Tickets at the door - $10</p>
           <p className={styles.cityState}>Newburyport, MA</p>
           <p className={styles.address}>25 Low St</p>
           <div className={styles.directionsButton}><a target="_blank" href="https://goo.gl/maps/GNckDZ5MLR3Ph5U77" rel="noreferrer">Directions</a></div>
